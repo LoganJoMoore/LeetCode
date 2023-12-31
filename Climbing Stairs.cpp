8974a69720a0903b14d0b149413e48cc7f671227
LoganJoMoore <155145036+LoganJoMoore@users.noreply.github.com>
2023-12-31 17:42:14 -0600

    Update Climbing Stairs.cpp

--- Climbing Stairs.cpp
+++ Climbing Stairs.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
+        if (n < 4) {
+            return n;
         }
-
-        vector<int> ans(n+1);
-        ans[0] = ans[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            ans[i] = ans[i-1] + ans[i-2];
-        }
-
-        return ans[n];
+        return climbStairs(n-1) + climbStairs(n-2);
     }
 };
