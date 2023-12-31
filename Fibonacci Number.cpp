6e9c0ee79aada740ce870ec6bea3be064e9f39f9
LoganJoMoore <155145036+LoganJoMoore@users.noreply.github.com>
2023-12-31 17:37:25 -0600

    Update Fibonacci Number.cpp

--- Fibonacci Number.cpp
+++ Fibonacci Number.cpp
@@ -1,17 +1,9 @@
 class Solution {
 public:
     int fib(int n) {
-        int fibIndex = 1;
-        int fibLast = 0;
-        
-        if (n == 0) {return 0;}
-
-        for (int i = 1; i < n; i++) {
-            int temp = fibIndex;
-            fibIndex += fibLast;
-            fibLast = temp;
+        if (n <= 1) {
+            return n;
         }
-
-        return fibIndex;
+        return fib(n-1) + fib(n-2);
     }
 };
