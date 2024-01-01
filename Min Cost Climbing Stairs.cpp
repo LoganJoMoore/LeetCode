1ed3a1bf934295bdfe4e4b4412b4d502bec6f1a2
LoganJoMoore <155145036+LoganJoMoore@users.noreply.github.com>
2023-12-31 18:01:25 -0600

    Update Min Cost Climbing Stairs.cpp

--- Min Cost Climbing Stairs.cpp
+++ Min Cost Climbing Stairs.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-
         for (int i = 2; i < n; i++) {
             cost[i] += min(cost[i-1], cost[i-2]);
         }
